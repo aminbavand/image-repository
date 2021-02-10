9cafa80a98c8cebc66617116617153a368857f9d
Amin Bavand <bavand@ualberta.ca>
2021-02-09 23:59:44 -0700

    update files

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from "react-router-dom";
 
-class Main extends Component {
+class Signup extends Component {
   state = {
     username: '',
     password: '',
